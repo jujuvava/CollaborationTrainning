010326148643b67081318723cf185e8e6d26e515
jujuvava <junavandriyani@gmail.com>
2019-10-14 20:00:25 +0700

    Mengedit file index.php

--- Index.php
+++ Index.php
@@ -1,6 +1,6 @@
 <?php
 echo "<h2> Selamat berlatih menjadi kolaborator </h2>";
 echo "<h3>Isi Nama dan Nim Anda di bawah baris code ini</h3>";
-echo "<h3>Ng. Satria Utomo W.P.(155610024)</h3>"
+echo "<h3>Junaidi,Rifqi anugrah, ade candrawan zona, (195410255,195410238,195410254)</h3>"
 
 ?>
